08e0e874cd6fac4e3e7f7cecacecda1cd5d0fedf
Mate Marschalko <webondevices@gmail.com>
2017-01-17 21:55:48 +0000

    logging data to text file

---
+++ temp-log-json/temp-test.js
@@ -0,0 +1,37 @@
+var SerialPort = require("serialport");
+var port = new SerialPort("/dev/tty.x", {
+	parser: SerialPort.parsers.readline('\n')
+});
+
+var fs = require('fs');
+
+var lastMoment = new Date();
+var logIntervalMinutes = 15;
+
+console.log('Initialising...');
+
+port.on('open', function() {
+	console.log('Opened port...');
+	
+	port.on('data', function(data) {
+		var sensorData = tryParseJson(data);
+		var moment = new Date();
+
+		if (moment.getTime() - lastMoment.getTime() > logIntervalMinutes * 60 * 1000) {
+			lastMoment = moment;
+			fs.appendFile('log.txt', (sensorData.temperature + ' , ' +  sensorData.humidity + ' , ' + moment + '\n'), function (err) {
+			    if (err) return console.log(err);
+			    console.log('Logged data: ', moment);
+			});
+		}
+	});
+});
+
+function tryParseJson(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return JSON.parse(str);
+}
