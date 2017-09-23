bb8feab24394a0dc36c74882a78055a9b8bd6b55
Mate Marschalko <webondevices@gmail.com>
2017-09-23 17:03:37 +0100

    Sorting folders

--- car-obstacle-avoid/car-obstacle-avoid.cpp
+++ car-obstacle-avoid/car-obstacle-avoid.cpp
@@ -1,13 +1,13 @@
 #include <NewPing.h>
 
 // Set motor control pins
-int motorLeftSpeed = 7;
-int motorLeftForward = 8;
-int motorLeftBackward = 9;
+int motorLeftSpeed = 6;
+int motorLeftForward = 9;
+int motorLeftBackward = 8;
 
-int motorRightSpeed = 12;
-int motorRightForward = 10;
-int motorRightBackward = 11;
+int motorRightSpeed = 11;
+int motorRightForward = 12;
+int motorRightBackward = 10;
 
 // Set sonar sensor pins
 int trigger = 3;
@@ -31,68 +31,61 @@ void setup() {
     pinMode(motorRightBackward, OUTPUT);
 }
 
-void loop() {
-    
-    // If sonar sensor 
-    if
+void turnLeft(int speed) {
+
+    // Set left motor to backward direction
+    digitalWrite(motorLeftForward, LOW);
+    digitalWrite(motorLeftBackward, HIGH);
+    // Set left motor speed to maximum (0 - 255)
+    analogWrite(motorLeftSpeed, speed);
+
+    // Set right motor to forward direction
+    digitalWrite(motorRightForward, HIGH);
+    digitalWrite(motorRightBackward, LOW);
+    // Set right motor speed to maximum (0 - 255)
+    analogWrite(motorRightSpeed, speed);
 }
 
-// void demoOne() {
+void turnRight(int speed) {
+    
+    // Set left motor to forward direction
+    digitalWrite(motorLeftForward, HIGH);
+    digitalWrite(motorLeftBackward, LOW);
+    // Set left motor speed to maximum (0 - 255)
+    analogWrite(motorLeftSpeed, speed);
 
+    // Set right motor to backward direction
+    digitalWrite(motorRightForward, LOW);
+    digitalWrite(motorRightBackward, HIGH);
+    // Set right motor speed to maximum (0 - 255)
+    analogWrite(motorRightSpeed, speed);
+}
 
+void moveForward(int speed) {
+    
+    // Set left motor to forward direction
+    digitalWrite(motorLeftForward, HIGH);
+    digitalWrite(motorLeftBackward, LOW);
+    // Set left motor speed to maximum (0 - 255)
+    analogWrite(motorLeftSpeed, speed);
 
-//   // this function will run the motors in both directions at a fixed speed
-//   // turn on motor A
-//   digitalWrite(motorLeftForward, HIGH);
-//   digitalWrite(motorLeftBackward, LOW);
-//   // set speed to 200 out of possible range 0~255
-//   analogWrite(motorLeftSpeed, 200);
-//   // turn on motor B
-//   digitalWrite(motorRightForward, HIGH);
-//   digitalWrite(motorRightBackward, LOW);
-//   // set speed to 200 out of possible range 0~255
-//   analogWrite(motorRightSpeed, 200);
-//   delay(2000);
-//   // now change motor directions
-//   digitalWrite(motorLeftForward, LOW);
-//   digitalWrite(motorLeftBackward, HIGH);  
-//   digitalWrite(motorRightForward, LOW);
-//   digitalWrite(motorRightBackward, HIGH); 
-//   delay(2000);
-//   // now turn off motors
-//   digitalWrite(motorLeftForward, LOW);
-//   digitalWrite(motorLeftBackward, LOW);  
-//   digitalWrite(motorRightForward, LOW);
-//   digitalWrite(motorRightBackward, LOW);
-// }
-// void demoTwo()
-// {
-//   // this function will run the motors across the range of possible speeds
-//   // note that maximum speed is determined by the motor itself and the operating voltage
-//   // the PWM values sent by analogWrite() are fractions of the maximum speed possible 
-//   // by your hardware
-//   // turn on motors
-//   digitalWrite(motorLeftForward, LOW);
-//   digitalWrite(motorLeftBackward, HIGH);  
-//   digitalWrite(motorRightForward, LOW);
-//   digitalWrite(motorRightBackward, HIGH); 
-//   // accelerate from zero to maximum speed
-//   for (int i = 0; i < 256; i++)
-//   {
-//     analogWrite(motorLeftSpeed, i);
-//     analogWrite(motorRightSpeed, i);
-//     delay(20);
-//   } 
-//   // decelerate from maximum speed to zero
-//   for (int i = 255; i >= 0; --i)
-//   {
-//     analogWrite(motorLeftSpeed, i);
-//     analogWrite(motorRightSpeed, i);
-//     delay(20);
-//   } 
-//   // now turn off motors
-//   digitalWrite(motorLeftForward, LOW);
-//   digitalWrite(motorLeftBackward, LOW);  
-//   digitalWrite(motorRightForward, LOW);
-//   digitalWrite(motorRightBackward, LOW);  
-// }
+    // Set right motor to forward direction
+    digitalWrite(motorRightForward, HIGH);
+    digitalWrite(motorRightBackward, LOW);
+    // Set right motor speed to maximum (0 - 255)
+    analogWrite(motorRightSpeed, speed);
+}
+
+void loop() {
+
+    // If mesured distance is below threshold
+    if (sonar.ping_cm() < turnThreshold / 10) {
+
+        // Turn away
+        turnLeft(255);
+
+    // If measured distance is over threshold
+    } else {
+        moveForward(255);
+    }
+}
